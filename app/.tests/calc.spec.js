1a0d095845ae61ff2bc4f061e20d99c9548c69a2
roman <roman.pavlenko.job@gmail.com>
2024-10-09 12:53:47 +0300

    added linter

--- app/.tests/calc.spec.js
+++ app/.tests/calc.spec.js
@@ -1,9 +1,16 @@
 const { calculate } = require("../lib/CalcFromString")
 
-test("check basic math", () => {
-  expect(calculate("2 + 2")).toBe("4");
-  expect(calculate("2 + 2 + 2")).toBe("6");
-  expect(calculate("2 - 2")).toBe("0");
-  expect(calculate("2 * 2")).toBe("4");
-  expect(calculate("2 / 2")).toBe("1");
+describe("check basic math", () => {
+  test("check basic math. Two numbers", () => {
+    expect(calculate("2 + 2")).toBe("4");
+    expect(calculate("2 - 2")).toBe("0");
+    expect(calculate("2 * 2")).toBe("4");
+    expect(calculate("2 / 2")).toBe("1");
+  })
+  test("check basic math. Three numbers", () => {
+    expect(calculate("2 + 2 + 2")).toBe("6");
+    expect(calculate("2 - 2 - 2")).toBe("-2");
+    expect(calculate("2 * 2 * 2")).toBe("8");
+    expect(calculate("4 / 2 / 2")).toBe("1");
+  })
 })
--- app/components/Blocks/SearchBlock.tsx
+++ app/components/Blocks/SearchBlock.tsx
@@ -1,20 +1,22 @@
 "use client"
 
 interface Props {
+  name: string,
   array: number[],
   searchedElement: number,
   func: (arg : number[], element: number) => number
 }
 
-export function SearchBlock({array,searchedElement, func} : Props) {
+export function SearchBlock({name, array,searchedElement, func} : Props) {
   return(
     <div>
+      {`${name}`}
       <button
         onClick={() => {
           func(array, searchedElement)
         }}
       >
-
+        Check how much iteration it takes
       </button>
     </div>
   )
